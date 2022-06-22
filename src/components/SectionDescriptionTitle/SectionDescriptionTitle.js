84ec40ef586e435e66f67e399c7fa4ff888046fe
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2022-06-22 12:35:21 -0400

    trying to solve css module issue on netlify deploy

--- src/components/SectionDescriptionTitle/SectionDescriptionTitle.js
+++ src/components/SectionDescriptionTitle/SectionDescriptionTitle.js
@@ -2,7 +2,7 @@ import styles from './sectiondescriptiontitle.module.css';
 
 const SectionDescriptionTitle = ({ children }) => {
   return (
-    <p className={styles['title']}>{children}</p>
+    <p className={styles['desc-title']}>{children}</p>
   );
 }
 
